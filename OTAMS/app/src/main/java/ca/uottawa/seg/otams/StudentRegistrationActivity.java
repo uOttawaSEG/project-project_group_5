6c6ba5c5e1a3f2d85fa371ea698fb9782fc5f59a
zahabiasaify-debug <zahabia.saify@gmail.com>
2025-10-27 22:00:13 -0400

    After the tutor presses register button we should go back to the login page

--- OTAMS/app/src/main/java/ca/uottawa/seg/otams/StudentRegistrationActivity.java
+++ OTAMS/app/src/main/java/ca/uottawa/seg/otams/StudentRegistrationActivity.java
@@ -142,11 +142,9 @@ public class StudentRegistrationActivity extends AppCompatActivity {
             // setContentView(R.layout.activity_welcome_page);//redirect to welcome page
 
             // Set the next page as the welcome page
-            Intent intent = new Intent(StudentRegistrationActivity.this, WelcomePageActivity.class);
+            Intent intent = new Intent(StudentRegistrationActivity.this, MainActivity.class);
 
             // Pass the user's role to the next page so it can be displayed there
-            intent.putExtra("role", "Student");
-
             // Send the user to the welcome page
             startActivity(intent);
         }
--- OTAMS/app/src/main/java/ca/uottawa/seg/otams/TutorRegistrationActivity.java
+++ OTAMS/app/src/main/java/ca/uottawa/seg/otams/TutorRegistrationActivity.java
@@ -146,11 +146,9 @@ public class TutorRegistrationActivity extends AppCompatActivity {
             reference.child(numberInput).setValue(tutorRegistration);
 
             // Set the next page as the welcome page
-            Intent intent = new Intent(TutorRegistrationActivity.this, WelcomePageActivity.class);
+            Intent intent = new Intent(TutorRegistrationActivity.this, MainActivity.class);
 
             // Pass the user's role to the next page so it can be displayed there
-            intent.putExtra("role", "Tutor");
-
             // Send the user to the welcome page
             startActivity(intent);
         }
